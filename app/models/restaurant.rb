a010823559f5aefb18158ddffe1500c882db6729
Ben Slater <ben@overmation.com>
2015-05-21 16:16:21 +0100

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ActiveRecord::Base
   	  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   	  has_many :reviews
+
+  	  validates :name, :address, :phone, :website, :image, presence: true
+  	  validates :website, format: { with: /\Ahttps?:\/\/.*\z/, message: "must start with http:// or https://" }
 end
